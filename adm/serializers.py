5ea941b690585da671e3e2da3e144f7b8ee35bd1
marcos afonso <afonso-vieira@hotmail.com>
2021-02-13 19:25:44 -0300

    update create venda serializer pronto

--- adm/serializers.py
+++ adm/serializers.py
@@ -88,17 +88,69 @@ class VendaSerializer(serializers.ModelSerializer):
     #         fornec = ItemVenda.objects.create(**item, venda=venda)
 
     # # """ Criar metodo que simular formset com o cadastro de itens da venda """
-    # def create(self, validated_data):
-    #     print(validated_data)
-    #     itens_venda = validated_data['itemvenda_set']
-    #     del validated_data['itemvenda_set']
+    def calcula_comissao_da_venda(self, venda):
+        id_venda = venda.id
+        #pega hora atual:
+        hora_atual = datetime.today().time().replace(second=0, microsecond=0)
+        # intervalo um
+        inicio_intervalo_1 =  datetime.strptime('00:00', '%H:%M').time()
+        fim_intervalo_1 =  datetime.strptime('12:00', '%H:%M').time()
+        # intervalo dois
+        inicio_intervalo_2=  datetime.strptime('12:01', '%H:%M').time()
+        fim_intervalo_2 =  datetime.strptime('23:59', '%H:%M').time()
 
-    #     venda = Venda.objects.create(**validated_data)
+        intervalo_1_ativo = False
+        intervalo_2_ativo = False
 
-    #     self.cria_itens_venda(itens_venda, venda)
+        # Se vender entre 00:00 e 12:00 a comissão máxima é de 5% por item venda.
+        if hora_atual >= inicio_intervalo_1 and hora_atual <= fim_intervalo_1:
+            print("antes do meio-dia")
+            intervalo_1_ativo = True
+        
+        # Venda entre 12:01 e 23:59 a comissão é no mínimo 4%.
+        elif hora_atual >= inicio_intervalo_2 and hora_atual <= fim_intervalo_2:
+            print("depois do meio-dia HAHA")
+            intervalo_2_ativo = True
 
-    #     venda.save()
+        lista_item_venda = ItemVenda.objects.filter(venda_id=id_venda)
 
+        # itera sobre os itens venda, verificando a restrição de comissao e recalcula.
+        for item_venda in lista_item_venda:
+
+            if intervalo_1_ativo:
+                # entre 00:00 e 12:00 a comissão máxima é de 5% por item venda.
+                # se passar do maximo, aplicar o maximo e recalcular comissao do item
+                if item_venda.produto_servico.comissao > Decimal(5.0):
+                    item_venda.calcula_total_comissao_item(comissao_reaplicada=Decimal(5.0))
+                else:
+                    item_venda.calcula_total_comissao_item()
+            
+            elif intervalo_2_ativo:
+                # entre 12:01 e 23:59 a comissão é no mínimo 4%.
+                # se menor que 4%, aplicar o minimo e recalcular comissao do item
+                if item_venda.produto_servico.comissao < Decimal(4.0):
+                    print("Produto nao atinge comissao minima")
+                    item_venda.calcula_total_comissao_item(comissao_reaplicada=Decimal(4.0))
+                else:
+                    print("mais do que o minimo!!")
+                    item_venda.calcula_total_comissao_item()
+
+            item_venda.save()
+
+    def create(self, validated_data):
+
+        venda_obj = Venda.objects.create(**validated_data)
+        situacao_atualizada = venda_obj.situacao
+
+        # ao finalizar venda, verificar restriçoes de horario para dar comissão no item da venda:
+        # TODO: pode nao ser necessaria essa execucao de calculo_comissao:
+        if venda_obj.situacao == FINALIZADO:
+            print("pronto para calcular comissao no create")
+            self.calcula_comissao_da_venda(venda_obj)
+            print("calculou!!!!")
+
+        return venda_obj
+        
     # # """ Método que atualiza venda, e seus respectivos itens.
     # #     Atualiza, quando tem o id, quando não, cria um novo item para aquela venda.
     # # """
@@ -108,55 +160,15 @@ class VendaSerializer(serializers.ModelSerializer):
         situacao_atualizada = validated_data['situacao']
 
         intervalo_1_ativo = False
-        intervalo_1_ativo = False
+        intervalo_2_ativo = False
 
-        venda_old = Venda.objects.filter(id=id_venda).first()
-        # ao finalizar venda, verificar restriçoes de horario para dar comissão:
-        if situacao_atualizada == FINALIZADO and venda_old.situacao != situacao_atualizada:
+        venda_atual = Venda.objects.filter(id=id_venda).first()
 
-            #pega hora atual:
-            hora_atual = datetime.today().time().replace(second=0, microsecond=0)
-            # intervalo um
-            inicio_intervalo_1 =  datetime.strptime('00:00', '%H:%M').time()
-            fim_intervalo_1 =  datetime.strptime('12:00', '%H:%M').time()
-            # intervalo dois
-            inicio_intervalo_2=  datetime.strptime('12:01', '%H:%M').time()
-            fim_intervalo_2 =  datetime.strptime('23:59', '%H:%M').time()
-
-            # Se vender entre 00:00 e 12:00 a comissão máxima é de 5% por item venda.
-            if hora_atual >= inicio_intervalo_1 and hora_atual <= fim_intervalo_1:
-                print("antes do meio-dia")
-                intervalo_1_ativo = True
-            
-            # Venda entre 12:01 e 23:59 a comissão é no mínimo 4%.
-            elif hora_atual >= inicio_intervalo_2 and hora_atual <= fim_intervalo_2:
-                print("depois do meio-dia HAHA")
-                intervalo_2_ativo = True
-
-            lista_item_venda = ItemVenda.objects.filter(venda_id=id_venda)
-
-            # itera sobre os itens venda, verificando a restrição de comissao e recalcula.
-            for item_venda in lista_item_venda:
-
-                if intervalo_1_ativo:
-                    # entre 00:00 e 12:00 a comissão máxima é de 5% por item venda.
-                    # se passar do maximo, aplicar o maximo e recalcular comissao do item
-                    if item_venda.produto_servico.comissao > Decimal(5.0):
-                        item_venda.calcula_total_comissao_item(comissao_reaplicada=Decimal(5.0))
-                    else:
-                        item_venda.calcula_total_comissao_item()
-                
-                elif intervalo_2_ativo:
-                    # entre 12:01 e 23:59 a comissão é no mínimo 4%.
-                    # se menor que 4%, aplicar o minimo e recalcular comissao do item
-                    if item_venda.produto_servico.comissao < Decimal(4.0):
-                        print("Produto nao atinge comissao minima")
-                        item_venda.calcula_total_comissao_item(comissao_reaplicada=Decimal(4.0))
-                    else:
-                        print("mais do que o minimo!!")
-                        item_venda.calcula_total_comissao_item()
-
-                item_venda.save()
+        # ao finalizar venda, verificar restriçoes de horario para dar comissão no item da venda:
+        if situacao_atualizada == FINALIZADO :
+            self.calcula_comissao_da_venda(venda_atual)
+        else:
+            print("situacao finalizada repetiu")
 
         instance = super().update(instance, validated_data)
         return instance
